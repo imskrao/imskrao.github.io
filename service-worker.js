d7a642ec916f911c641ef2fb57908135c6ce0809
imskrao <raosantosh1997@gmail.com>
2021-07-04 10:56:21 +0530

    Service worker: Error Fixed

--- service-worker.js
+++ service-worker.js
@@ -28,13 +28,13 @@ const cacheAssets = [
 self.addEventListener('install', (e) => {
     console.log('Service Worker: Installed')
 
-    e.waitUntil(caches.open(cacheName)
-        .then(cache => {
-            console.log('Service Worker: Caching Files')
-            cache.addAll(cacheAssets)
-        })
-        .then(() => self.skipWaiting())
-    )
+    // e.waitUntil(caches.open(cacheName)
+    //     .then(cache => {
+    //         console.log('Service Worker: Caching Files')
+    //         cache.addAll(cacheAssets)
+    //     })
+    //     .then(() => self.skipWaiting())
+    // )
 })
 
 // call activate event
@@ -60,5 +60,19 @@ self.addEventListener('activate', (e) => {
 // Call fetch event
 self.addEventListener('fetch', e => {
     console.log('Service Worker: Fetching');
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+    e.respondWith(
+        fetch(e.request)
+            .then(res => {
+                // Make copy of response
+                const resClone = res.clone()
+                // open caches
+                caches.open(cacheName)
+                    .then(cache => {
+                        // Add response to cache
+                        cache.put(e.request, resClone)
+                    })
+                return res;
+            })
+            .catch(err => caches.match(e.request).then(res => res))
+    )
 })
