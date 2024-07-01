4ee0d0819e2ca974ee16f4e3eb50961f9dc499b7
imskrao <raosantosh1997@gmail.com>
2024-07-02 00:15:55 +0530

    Some string and image update

--- service-worker.js
+++ service-worker.js
@@ -27,21 +27,13 @@ const cacheAssets = [
 
 self.addEventListener('install', (e) => {
     console.log('Service Worker: Installed')
-
-    // e.waitUntil(caches.open(cacheName)
-    //     .then(cache => {
-    //         console.log('Service Worker: Caching Files')
-    //         cache.addAll(cacheAssets)
-    //     })
-    //     .then(() => self.skipWaiting())
-    // )
 })
 
 // call activate event
 
 self.addEventListener('activate', (e) => {
     console.log('Service Worker: Activated')
-    // Remove unwanted chaches 
+    // Remove unwanted chaches
     e.waitUntil(
         caches.keys()
             .then(cacheNames => {
